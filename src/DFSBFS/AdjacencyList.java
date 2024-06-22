e8ab51f6ff1dec031fa84a42a7c9f1852f8a7782
jiixon <jr0107380@gmail.com>
2024-06-22 19:43:15 +0900

    24.06.22 Upload

--- src/DFSBFS/AdjacencyList.java
+++ src/DFSBFS/AdjacencyList.java
@@ -1,45 +1,53 @@
 package DFSBFS;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 class Node {
     private int index;
-    private int distance;
+    private int cost;
 
-    public Node(int index, int distance) {
+    public Node(int index, int cost) {
         this.index = index;
-        this.distance = distance;
+        this.cost = cost;
     }
 
-    public void show() {
-        System.out.print("(" + this.index + ", " + this.distance + ")");
+    @Override
+    public String toString() {
+        return "[" + index + ", "
+                + "" + cost + "]";
     }
 }
 
 public class AdjacencyList {
-    public static ArrayList<ArrayList<Node>> graph = new ArrayList<ArrayList<Node>>();
     public static void main(String[] args) {
-        int rows = 3;
+        Scanner sc = new Scanner(System.in);
+        // V = 정점의 개수, E = 간선의 개수.
+        int V = sc.nextInt();
+        int E = sc.nextInt();
 
-        //그래프 초기화
-        for (int i = 0; i < rows; i++) {
+        // 1차원 리스트를 만든다.
+        // 인덱스의 번호 = 노드의 번호 이기 때문에, 1차원 리스트의 크기를 임의로 1 늘려서 정의한다(스킬).
+        ArrayList<ArrayList<Node>> graph = new ArrayList<ArrayList<Node>>();
+        for (int i = 0; i < V + 1; i++) {
             graph.add(new ArrayList<Node>());
         }
 
-        //그래프의 연결된 노드 저장
-        graph.get(0).add(new Node(1, 7));
-        graph.get(0).add(new Node(2, 5));
+        int a;
+        int b;
+        int cost;
+        for (int i = 0; i < E; i++) {
+            a = sc.nextInt();
+            b = sc.nextInt();
+            cost = sc.nextInt();
 
-        graph.get(1).add(new Node(0, 7));
-
-        graph.get(2).add(new Node(0, 5));
-
-        //그래프 출력
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < graph.get(i).size(); j++) {
-                graph.get(i).get(j).show();
-            }
-            System.out.println();
+            graph.get(a).add(new Node(b, cost));
+            // 만일, 무향 그래프라면 반대의 상황도 추가해 준다.
+            // graph.get(b).add(new Node(a, cost));
         }
+
+        // 인접 행렬 출력
+        System.out.println(graph);
+        sc.close();
     }
 }
